5cedebae65b34acca307239e7f32ca428b14f28f
sarah <sarahphan2314@gmail.com>
2022-03-04 13:10:10 +0700

    Form chưa validation

--- airbnbapp/src/container/HomeTemplate/TrangChu/index.js
+++ airbnbapp/src/container/HomeTemplate/TrangChu/index.js
@@ -1,48 +1,105 @@
 import React from 'react'
 import "./style.css"
-import Input from '@mui/material/Input';
-import InputAdornment from '@mui/material/InputAdornment';
-import FormControl from '@mui/material/FormControl';
-import Grid from '@mui/material/Grid';
-import PersonPinCircleSharpIcon from '@mui/icons-material/PersonPinCircleSharp';
-import InputLabel from '@mui/material/InputLabel';
-import MenuItem from '@mui/material/MenuItem';
-import Select from '@mui/material/Select';
+import { Row, Col } from "antd"
+import { Form, InputNumber, Select, DatePicker } from "antd"
+import { Avatar, Space, Dropdown, Button, Menu } from 'antd'
+import { EnvironmentOutlined, UnorderedListOutlined, UserOutlined } from "@ant-design/icons"
 
 export default function TrangChu() {
-  const [location, setLocation] = React.useState('');
+  const TimeRelatedForm = () => {
+    const onFinished = (fieldsValue) => {
+      const values = {
+        ...fieldsValue,
+        'check-in-date': fieldsValue['check-in-date'].format('DD-MM-YYYY'),
+        'check-out-date': fieldsValue['check-out-date'].format('DD-MM-YYYY'),
+        'number-customer': fieldsValue['number-customer'],
+        'select-location': fieldsValue['select-location']
+      }
+      console.log("values: ", values)
+    }
+    return (
+      <Form
+        className='formTimKiemNoiDung'
+        layout='vertical'
+        onFinish={onFinished}
+      >
+        <Form.Item 
+        label="Địa điểm" 
+        >
+          <Select name="select-location" suffixIcon={<EnvironmentOutlined />} style={{ width: "90%" }}>
+            <Select.Option value="HoChiMinh">Ho Chi Minh</Select.Option>
+          </Select>
+        </Form.Item>
+        <Form.Item label="Ngày nhận phòng" name="check-in-date" rules={
+          [
+            {
+              required: true,
+              message: "Hãy chọn ngày nhận phòng"
+            }
+          ]
+        }>
+          <DatePicker style={{ width: "90%" }} />
+        </Form.Item>
+        <Form.Item label="Ngày trả phòng" name="check-out-date">
+          <DatePicker style={{ width: "90%" }} />
+        </Form.Item>
+        <Form.Item label="Số lượng khách">
+          <Form.Item name="number-customer" noStyle>
+            <InputNumber min={1} max={20} style={{ width: "90%" }} />
+          </Form.Item>
+        </Form.Item>
+        <Button htmlType="submit" className='buttonSubmit'>
+          Submit
+        </Button>
+      </Form>
+    )
+  }
 
-  const handleChange = (event) => {
-    setLocation(event.target.value);
-  };
+  const menu = (
+    <Menu>
+      <Menu.Item>
+        <a target="_blank" rel="noopener noreferrer" href="https://www.antgroup.com">
+          1st menu item
+        </a>
+      </Menu.Item>
+      <Menu.Item>
+        <a target="_blank" rel="noopener noreferrer" href="https://www.aliyun.com">
+          2nd menu item
+        </a>
+      </Menu.Item>
+      <Menu.Item>
+        <a target="_blank" rel="noopener noreferrer" href="https://www.luohanacademy.com">
+          3rd menu item
+        </a>
+      </Menu.Item>
+    </Menu>
+  );
+
   return (
     <div className='trangChuCarousel'>
-      <Grid container>
-        <Grid item xs={6}>
-          <h1>fsdfsd</h1>
-        </Grid>
-        <Grid item xs={6} className="formTimKiem">
-          <div>
-            <FormControl sx={{ width: 130 }}>
-              <Select
-                value={location}
-                onChange={handleChange}
-                displayEmpty
-              >
-                <MenuItem value="">
-                  <em>Địa điểm</em>
-                </MenuItem>
-                <MenuItem value={10}>Ten</MenuItem>
-                <MenuItem value={20}>Twenty</MenuItem>
-                <MenuItem value={30}>Thirty</MenuItem>
-              </Select>
-            </FormControl>
-          </div>
-          <div>
-            
-          </div>
-        </Grid>
-      </Grid>
+      <div className='header'>
+        <Space wrap>
+          <Dropdown overlay={menu}>
+            <div className='userNav'>
+              <UnorderedListOutlined style={{ fontSize: 30, paddingRight: 4 }} />
+              <Avatar size="large" icon={<UserOutlined />} />
+            </div>
+          </Dropdown>
+        </Space>
+      </div>
+      <div className='carouselContent'>
+        <Row>
+          <Col span={12}>
+            <h1>Airbnb</h1>
+          </Col>
+          <Col span={12}>
+            <div className='formTimKiem'>
+              {TimeRelatedForm()}
+            </div>
+          </Col>
+        </Row>
+      </div>
+
     </div>
   )
 }
