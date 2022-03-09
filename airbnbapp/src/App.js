54c914244897d4588a6ea7d8bba3acc7bd871533
NguyenPhi <nguyenphidhbk@gmail.com>
2022-03-09 22:43:37 +0700

    update  code

--- airbnbapp/src/App.js
+++ airbnbapp/src/App.js
@@ -1,20 +1,22 @@
-import './App.css';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import { createBrowserHistory } from 'history';
-import { Suspense } from 'react';
-import { renderRouteHome } from './routes';
-import Loading from './components/loading';
-import PageNotFound from './container/PageNotFound';
+import "./App.css";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { createBrowserHistory } from "history";
+import { Suspense } from "react";
+import { renderRouteHome } from "./routes";
+import { renderRoutesAdmin } from "./routes";
+import Loading from "./components/loading";
+import PageNotFound from "./container/PageNotFound";
 
-export const history = createBrowserHistory()
+export const history = createBrowserHistory();
 
 function App() {
   return (
-    <Suspense history={history} fallback={<Loading/>}>
+    <Suspense history={history} fallback={<Loading />}>
       <BrowserRouter>
         <Switch>
           {renderRouteHome()}
-          <Route path='' component={PageNotFound}/>
+          {renderRoutesAdmin()}
+          <Route path="" component={PageNotFound} />
         </Switch>
       </BrowserRouter>
     </Suspense>
--- airbnbapp/src/routes/index.js
+++ airbnbapp/src/routes/index.js
@@ -3,22 +3,47 @@ import HomeTemplate from "../container/HomeTemplate";
 import AdminTemplate from "../container/AdminTemplate";
 
 const routeHome = [
-    {
-        exact: true,
-        path: "/",
-        component: lazy(() => import("../container/HomeTemplate/TrangChu"))
-    }
-]
+  {
+    exact: true,
+    path: "/",
+    component: lazy(() => import("../container/HomeTemplate/TrangChu")),
+  },
+];
+
+const routesAdmin = [
+  //Admin
+  {
+    exact: false,
+    path: "/admin",
+    // component: Dashboard,
+    component: lazy(() =>
+      import("../container/AdminTemplate/AdminPage/index.js")
+    ),
+  },
+];
 
 export const renderRouteHome = () => {
-    return routeHome?.map((route, index) => {
-        return(
-            <HomeTemplate
-            key={index}
-            exact={route.exact}
-            path={route.path}
-            component={route.component}
-            />
-        )
-    })
-}
+  return routeHome?.map((route, index) => {
+    return (
+      <HomeTemplate
+        key={index}
+        exact={route.exact}
+        path={route.path}
+        component={route.component}
+      />
+    );
+  });
+};
+
+export const renderRoutesAdmin = () => {
+  return routesAdmin?.map((route, index) => {
+    return (
+      <AdminTemplate
+        key={index}
+        exact={route.exact}
+        path={route.path}
+        component={route.component}
+      />
+    );
+  });
+};
